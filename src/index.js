c6a250b93d9df0839de46d9cbbeb9ca7e115bb5a
wxdong5211 <wxdong5211@gmail.com>
2018-05-05 15:54:11 +0800

    add http get

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
 console.log('hi123asdasd');
+const x = http_1.default.get('http://www.baidu.com', res => {
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+        try {
+            // const parsedData = JSON.parse(rawData);
+            console.log(rawData);
+        }
+        catch (e) {
+            console.error(e.message);
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import fs from 'fs'
+import http from 'http'
 
 console.log('hi123asdasd')
+const x = http.get('http://www.baidu.com', res => {
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      // const parsedData = JSON.parse(rawData);
+      console.log(rawData);
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+})
