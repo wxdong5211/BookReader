f6f0b7b5412f72d24568e3eb20e0bb41a7efb5ce
wxdong5211 <wxdong5211@gmail.com>
2018-05-26 18:19:50 +0800

    move func update to reader

--- src/index.js
+++ src/index.js
@@ -2,104 +2,10 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const request_1 = __importDefault(require("./request"));
-const file_1 = __importDefault(require("./file"));
-const api = __importStar(require("./api"));
-const sleep = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
-const readHtml = async (url) => {
-    const option = request_1.default.parseUrl(url);
-    let req = await request_1.default.request(option);
-    return req;
-};
-const readChar = (char) => {
-    return readHtml(char.url);
-};
-const subDirHtml = (book, req) => {
-    const start = book.block.dirStart;
-    const end = book.block.dirEnd;
-    req = req.substr(req.indexOf(start) + start.length);
-    req = req.substr(0, req.indexOf(end));
-    return req;
-};
-const subCharHtml = (book, req) => {
-    const start = book.block.charStart;
-    const end = book.block.charEnd;
-    req = req.substr(req.indexOf(start) + start.length);
-    req = req.substr(0, req.indexOf(end));
-    return req;
-};
-const parseCharLink = (tag, idx) => {
-    const hrefStart = 'href="';
-    let href = tag.substr(tag.indexOf(hrefStart) + hrefStart.length);
-    href = href.substr(0, href.indexOf('"'));
-    const title = tag.replace(/<\/?[^>]*>/g, '');
-    const charcter = {
-        url: href,
-        title: title,
-        create: new Date(),
-        disOrder: idx,
-        order: idx,
-        state: api.CharcterState.Init
-    };
-    return charcter;
-};
-const parseDir = (book, req) => {
-    const dirHtml = req.match(/<a.*href=".*".*>.*<\/a>/gi);
-    return dirHtml ? dirHtml.map(parseCharLink) : [];
-};
-const readDir = async (book) => {
-    let req = await readHtml(book.url);
-    return parseDir(book, subDirHtml(book, req));
-};
-const updateDir = async (book) => {
-    try {
-        const chars = await readDir(book);
-        writeChars('data/books/chars.json', chars);
-        for (let x in chars) {
-            await sleep(100);
-            console.log(new Date());
-            console.log(chars[x]);
-            const data = await readChar(chars[x]);
-            writeCharData('data/books/chars/' + x + '.json', subCharHtml(book, data));
-        }
-    }
-    catch (e) {
-        console.log('problem with request: ' + e.message);
-    }
-};
-const writeChars = (path, chars) => {
-    writeJson(path, { chars });
-};
-const writeChar = (path, char) => {
-    writeJson(path, char);
-};
-const writeCharData = (path, data) => {
-    writeJson(path, { data });
-};
-const writeBook = (path, book) => {
-    writeJson(path, book);
-};
-const writeJson = (path, data) => {
-    file_1.default.writeFile(path, JSON.stringify(data, null, 2));
-};
-const init = () => {
-    return {
-        "sites": file_1.default.readJsonDir('data/sites'),
-        "books": file_1.default.readJsonDir('data/books')
-    };
-};
+const reader_1 = __importDefault(require("./reader"));
 const test = () => {
-    const book = init();
-    console.log(book);
-    updateDir(book.books[0]);
-    writeBook('data/test.json', book.books[0]);
+    const r = new reader_1.default();
+    r.updateAll();
 };
 test();
--- src/reader.ts
+++ src/reader.ts
@@ -2,12 +2,113 @@ import request from './request'
 import file from './file'
 import * as api from './api'
 
+const sleep = (ms: number): Promise<void> => new Promise<void>((resolve,reject) => setTimeout(resolve, ms));
+
+const readHtml = async(url: string) : Promise<string> => {
+  const option = request.parseUrl(url);
+  let req = await request.request(option);
+  return req;
+}
+
+const readChar = (char: api.Charcter) : Promise<string>  => {
+  return readHtml(char.url);
+}
+
+const subDirHtml = (book:api.Book, req: string): string => {
+  const start = book.block.dirStart;
+  const end = book.block.dirEnd;
+  req = req.substr(req.indexOf(start) + start.length);
+  req = req.substr(0, req.indexOf(end));
+  return req;
+}
+
+const subCharHtml = (book:api.Book, req: string): string => {
+  const start = book.block.charStart;
+  const end = book.block.charEnd;
+  req = req.substr(req.indexOf(start) + start.length);
+  req = req.substr(0, req.indexOf(end));
+  return req;
+}
+
+const parseCharLink = (tag: string, idx: number): api.Charcter => {
+  const hrefStart = 'href="'
+  let href = tag.substr(tag.indexOf(hrefStart) + hrefStart.length);
+  href = href.substr(0, href.indexOf('"'))
+  const title  = tag.replace(/<\/?[^>]*>/g,'')
+  const charcter = {
+    url : href,
+    title : title,
+    create : new Date(),
+    disOrder : idx,
+    order : idx,
+    state : api.CharcterState.Init
+  }
+  return charcter
+}
+
+const parseDir = (book:api.Book, req: string) : Array<api.Charcter> => {
+  const dirHtml = req.match(/<a.*href=".*".*>.*<\/a>/gi)
+  return dirHtml ? dirHtml.map(parseCharLink) : [];
+}
+
+const readDir = async (book:api.Book) : Promise<Array<api.Charcter>>  => {
+  let req = await readHtml(book.url);
+  return parseDir(book, subDirHtml(book, req));
+}
+
+const updateDir = async (book:api.Book) => {
+  try {
+    const chars = await readDir(book);
+    writeChars('data/books/chars.json', chars);
+    for (let x in chars) {
+      await sleep(100)
+      console.log(new Date())
+      console.log(chars[x])
+      const data = await readChar(chars[x])
+      writeCharData('data/books/chars/'+x+'.json', subCharHtml(book, data));
+    }
+  } catch (e) {
+    console.log('problem with request: ' + e.message);
+  }
+}
+
+const writeChars = (path: string, chars:Array<api.Charcter>) => {
+  writeJson(path, {chars})
+}
+
+const writeChar = (path: string, char:api.Charcter) => {
+  writeJson(path, char)
+}
+
+const writeCharData = (path: string, data:string) => {
+  writeJson(path, {data})
+}
+
+const writeBook = (path: string, book:api.Book) => {
+  writeJson(path, book)
+}
+
+const writeJson = (path: string, data:any) => {
+  file.writeFile(path, JSON.stringify(data, null, 2))
+}
+
+const init = () => {
+  return {
+    "sites": file.readJsonDir('data/sites'),
+    "books": file.readJsonDir('data/books')
+  }
+}
+
+const book = init()
+
+console.log(book)
+
 class ReaderImpl implements api.Reader {
   all(): api.Book[] {
-    throw new Error("Method not implemented.");
+    return book.books;
   }
   list(param: object): api.Book[] {
-    throw new Error("Method not implemented.");
+    return book.books;
   }
   get(id: string): api.Book {
     throw new Error("Method not implemented.");
@@ -22,7 +123,9 @@ class ReaderImpl implements api.Reader {
     throw new Error("Method not implemented.");
   }
   updateAll(): string {
-    throw new Error("Method not implemented.");
+    updateDir(book.books[0]);
+    writeBook('data/test.json', book.books[0])
+    return '123'
   }
 }
 
