336975cd16cfe7a6b93c21e2a930bd20a1564509
wxd <wxd@wxd.info>
2021-05-09 19:17:15 +0800

    save index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import factory from './factory'
 
-const test = () => {
+const test = async () => {
+  // await search('刺客之王')
+  await updateDirs(33, 49)
+  // await updateChars(49, 0)
+  // await exportChars(49, 0)
+}
+
+const search = async (name: string) => {
+  const r = factory.getReader()
+  const books = await r.search(name)
+  console.log(books)
+  if(books && books.length == 1){
+    r.add(books[0])
+  
+  }
+
+}
+
+const test1 = async () => {
   const r = factory.getReader()
   // r.updateAll()
-  const book = r.get(1)
+
+  const book = r.get(18)
   console.log('get book = ', book)
-  // const ret = book.updateDir()
-  // console.log('updateDir ret = ', ret)
+  // r.del(book)
+  const ret = await book.updateDir()
+  console.log('updateDir ret = ', ret)
   // const ret = book.getChars()
   // console.log('getChars ret = ', ret)
   // const ret1 = sortChars(ret)
@@ -16,8 +36,8 @@ const test = () => {
   // console.log('getChar ret = ', ret)
   // const ret = book.getCharsScope(1,3)
   // console.log('getCharsScope ret = ', ret)
-  // const idx = 953;
-  // const ret1 = book.updateChar(idx)
+  // const idx = 233;
+  // const ret1 = await book.updateChar(idx)
   // console.log('updateChar ret = ', ret1)
   // const ret2 = book.exportChar(idx)
   // console.log('exportChar ret = ', ret2)
@@ -26,16 +46,43 @@ const test = () => {
   // const ret = book.exportCharScope(1, 5)
   // console.log('exportCharScope ret = ', ret)
   // console.log('getCharsLength ret = ', book.getCharsLength())
-  // const ret = book.updateCharScope(953)
+  // const ret = await book.updateCharScope(691)
   // console.log('updateCharScope ret = ', ret)
   // const ret = book.exportCharScope(100)
   // console.log('exportCharScope ret = ', ret)
   // book.updateCharState(1, 1)
   // console.log('updateCharState')
-  // book.updateCharStateScope(0, 953)
+  // book.updateCharStateScope(0, 0)
   // console.log('updateCharStateScope')
-  const ret = book.exportTxtScope(953)
-  console.log('exportTxtScope ret = ', ret)
+  // const ret = book.exportTxtScope(691)
+  // console.log('exportTxtScope ret = ', ret)
+};
+
+const updateChars = async (idx: number, start: number) => {
+  const r = factory.getReader()
+  const book = r.get(idx)
+  const ret = await book.updateCharScope(start)
+  console.log(idx + ' ' +book.name + ' ' +book.id + ' start ' + start + ' updateCharScope ret = ', ret)
+  // const ret = book.exportTxtScope(691)
+  // console.log('exportTxtScope ret = ', ret)
+};
+
+const exportChars = async (idx: number, start: number) => {
+  const r = factory.getReader()
+  const book = r.get(idx)
+  const ret = await book.exportTxtScope(start)
+  console.log(idx + ' ' +book.name + ' ' +book.id + ' start ' + start + ' exportTxtScope ret = ', ret)
+};
+
+const updateDirs = async (start: number, end: number) => {
+  const r = factory.getReader()
+  for(let i = start; i <= end; i++){
+    const book = r.get(i)
+    if(book){
+      const ret = await book.updateDir()
+      console.log(i + ' ' +book.name + ' ' +book.id + ' updateDir ret = ', ret)
+    }
+  }
 };
 
 test();
